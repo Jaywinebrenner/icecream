1540a36a24c3d4e35a9369e94c475e7720ec39c3
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-28 20:36:52 -0700

    clean scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,15 @@
-// $(document).ready(function(){
-//   $("#icecreamForm").submit(function(event){
-//     event.preventDefault();
-//
-//     var blanks = ["1", "2", "3"];
-//     blanks.forEach(function(blank){
-//       var userInput = $("input" + blank).val();
-//
-//     $("output" + blank).append(userInput);
-//
-//
-//   });
-//   });
-// });
 
-
-
-
-
-
-
-
-
-
-
-
+// This only works if id of input and class of output has the same name
 $(document).ready(function(){
     $(".icecreamForm").submit(function(event){
         event.preventDefault();
         var blanks = ["ice1", "ice2", "ice3", "ice4", "ice5"];
 
         blanks.forEach(function(blank){
-          userInput = $("#" + blank).val();
-
-
+          var userInput = $("#" + blank).val();
           $("." + blank).append(userInput);
+        });
 
-
-
-        })
 
 
 
